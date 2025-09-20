8801857e627d98bcef8f2678e592ac569c13e8a3
Fahadboi66 <fahadzafarmayo123@gmail.com>
2025-09-20 18:43:56 +0500

    Refactor code

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from model import train_and_predict
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "ML Flask App Running!"
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
     result = train_and_predict(data["input"])
     return jsonify({"prediction": result})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+
 # Load dataset
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
@@ -11,6 +12,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Train model
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
+
 
 # Predict function
 def train_and_predict(sample):
